21899e89b3e8a1642ed4d430cbd0ee7098d88fb5
Krishan Gopal <kgc9901@gmail.com>
2022-05-04 23:42:52 +0530

    AmiSpace:
    * Completed SplashScreen Design.
    * Completed Intro Page
    * Completed SignUp Page
    * Completed SignIn Page
    * Authentication Done
    * Auto LogIn Done
    * Completed SignOut
    * Made Drawer
    * TODO: Phone Activity
    * TODO: Google Auth
    * TODO: Mobile Auth

--- app/src/main/java/in/kgdroid/amispace/activities/MainActivity.kt
+++ app/src/main/java/in/kgdroid/amispace/activities/MainActivity.kt
@@ -1,12 +1,74 @@
 package `in`.kgdroid.amispace.activities
 
 import `in`.kgdroid.amispace.R
+import `in`.kgdroid.amispace.databinding.ActivityMainBinding
+import `in`.kgdroid.amispace.databinding.ActivitySignUpBinding
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBar()
+
+        binding.navView.setNavigationItemSelectedListener(this)
+    }
+
+    private fun setupActionBar(){
+        val tool_bar_main: androidx.appcompat.widget.Toolbar = binding.toolBar.toolbarMainActivity
+        setSupportActionBar(tool_bar_main)
+        tool_bar_main.setNavigationIcon(R.drawable.ic_action_navigation)
+        tool_bar_main.setNavigationOnClickListener{
+            toggleDrawer()
+        }
+
+    }
+
+    private fun toggleDrawer(){
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            doubleBackToExit()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.nav_my_profile ->{
+                Toast.makeText(this@MainActivity, "My Profile", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_sign_out ->{
+                FirebaseAuth.getInstance().signOut()
+
+                val intent= Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
